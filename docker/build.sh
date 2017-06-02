03f35b22136792c17b85fd72e5edfd42fe9a55b8
Tom Desair <tom.desair@atmire.com>
2017-06-02 17:39:32 +0200

    Fix build file

--- docker/build.sh
+++ docker/build.sh
@@ -2,5 +2,5 @@
 if [ -z "$1" ]; then
   docker build -t atmire/dspace-angular .
 else
-  docker build -t atmire/dspace-server --build-arg $1 .
+  docker build -t atmire/dspace-angular --build-arg $1 .
 fi
